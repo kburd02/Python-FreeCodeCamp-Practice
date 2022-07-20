277220c6236caec76261f71bc5b1922236a0fca4
Ka'Pri Burden <kapriburden@gmail.com>
2022-07-19 21:03:18 -0500

    First commit

---
+++ Mad Libs Game.py
@@ -0,0 +1,12 @@
+#06/07/2022
+#Ka'Pri Burden
+
+color= input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity= input("Enter a celebrity: ")
+
+
+print("Roses are " + color)
+print(plural_noun +" are blue")
+print("I love " + celebrity)
+
